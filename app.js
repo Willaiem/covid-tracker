18f38ffc89063c7a987404cb9bc7b3eac09afd64
Willaiem <vvwill4u@gmail.com>
2020-12-04 20:53:47 +0100

    Added the get date function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const getCurrentCovidTotalReport = async () => {
+const getCovidTotalReportByDate = async () => {
     const getCurrentISODate = new Date().toISOString();
     const currentYear = getCurrentISODate.slice(0, 4);
     const currentMonth = getCurrentISODate.slice(5, 7);
@@ -12,6 +12,18 @@ const getCurrentCovidTotalReport = async () => {
     console.log("current date: ", currentDay, currentMonth, currentYear);
     return responseCovidReports.data.data;
 };
+
+const getDate = (date) => {
+    const getCurrentISODate = date
+        ? new Date(date).toISOString()
+        : new Date().toISOString();
+    const currentYear = getCurrentISODate.slice(0, 4);
+    const currentMonth = getCurrentISODate.slice(5, 7);
+    const currentDay = getCurrentISODate.slice(8, 10);
+    return new Object({ currentDay, currentMonth, currentYear });
+};
+
+const getCovidReportByCountryAndDate = async (ISO, countryName) => {};
 
 const selectAllCounters = () => {
     const counterElsList = [];
@@ -54,12 +66,13 @@ const initApp = async () => {
         date,
         deaths,
         recovered,
-    } = await getCurrentCovidTotalReport();
+    } = await getCovidTotalReportByDate();
     activeCounter.innerText = active;
     confirmedCounter.innerText = confirmed;
     deathsCounter.innerText = deaths;
     recoveredCounter.innerText = recovered;
     fillFilterWithCountriesData();
+    console.log(getDate());
 };
 
 initApp();
